1bb7f72a2d143e9d2c6bf366b127a37c41918470
fei <guanfei0052@gmail.com>
2024-11-24 11:41:03 -0500

    Fix a bug: interaction issues between controls on each row.

--- event.js
+++ event.js
@@ -1,4 +1,3 @@
-let addButton = document.createElement("button");
 function createEventElem(event) {
     const eventItemElem = document.createElement("tr");
     // event elem
@@ -9,19 +8,16 @@ function createEventElem(event) {
     // create td node
     const tdEventInput = document.createElement("td");
     const eventInput = document.createElement("input");
-    eventInput.required = true;
     tdEventInput.appendChild(eventInput);
     //add start date input field
     const tdStartDateInput = document.createElement("td");
     const startDateInput = document.createElement("input");
     startDateInput.type = "date";
-    startDateInput.required = true;
     tdStartDateInput.appendChild(startDateInput);
     //add end date input field
     const tdEndDateInput = document.createElement("td");
     const endDateInput = document.createElement("input");
     endDateInput.type = "date";
-    endDateInput.required = true;
     tdEndDateInput.appendChild(endDateInput);
     //set the input fields
     if (event !== undefined) {
@@ -42,10 +38,23 @@ function createEventElem(event) {
             startDate: startDateInput.value,
             endDate: endDateInput.value,
         };
-
         const res = await eventAPI.editEvent(event.id, newEvent);
-        const events = await eventAPI.getEvents();
-        renderEvents(events, 'event-list');
+        eventElem.textContent = `${newEvent.eventName}`;
+        startDateElem.textContent = `${newEvent.startDate}`;
+        endDateElem.textContent = `${newEvent.endDate}`;
+        eventItemElem.appendChild(eventElem);
+        eventItemElem.appendChild(startDateElem);
+        eventItemElem.appendChild(endDateElem);
+        eventItemElem.appendChild(deleteButton);
+        eventItemElem.appendChild(editButton);
+        tdEventInput.remove();
+        tdStartDateInput.remove();
+        tdEndDateInput.remove();
+        saveButton.remove();
+        cancelButton.remove();
+
+        //const events = await eventAPI.getEvents();
+        //renderEvents(events, 'event-list');
         return res.json;
     });
     //cancel button
@@ -60,12 +69,12 @@ function createEventElem(event) {
         saveButton.remove();
         cancelButton.remove();
         addButton.remove();
-        
-    eventItemElem.appendChild(eventElem);
-    eventItemElem.appendChild(startDateElem);
-    eventItemElem.appendChild(endDateElem);
-    eventItemElem.appendChild(deleteButton);
-    eventItemElem.appendChild(editButton);
+
+        eventItemElem.appendChild(eventElem);
+        eventItemElem.appendChild(startDateElem);
+        eventItemElem.appendChild(endDateElem);
+        eventItemElem.appendChild(deleteButton);
+        eventItemElem.appendChild(editButton);
     });
     //add delete button
     const deleteButton = document.createElement("button");
@@ -74,7 +83,7 @@ function createEventElem(event) {
         e.preventDefault();
         const res = await eventAPI.deleteEvent(event.id);
         const events = await eventAPI.getEvents();
-        renderEvents(events, "event-list");
+        eventItemElem.remove();
         return event.id;
     });
     //add edit button
@@ -94,56 +103,7 @@ function createEventElem(event) {
         eventItemElem.appendChild(saveButton);
         eventItemElem.appendChild(cancelButton);
     });
-    //add new event button
-    const addNewEvent = document.getElementById("addNew");
-    const eventNew = document.createElement("tr");
-   
-    addNewEvent.addEventListener("click", async (e) => {
-        e.preventDefault();
-        eventInput.value = "";
-        startDateInput.value = "";
-        endDateInput.value = "";
-        eventNew.appendChild(tdEventInput);
-        eventNew.appendChild(tdStartDateInput);
-        eventNew.appendChild(tdEndDateInput);
-        
-        addButton.textContent = "+";
-        eventNew.appendChild(addButton);
-        //prevent the button from being clicked multiple times
-        addButton.replaceWith(addButton.cloneNode(true));
-        //reset the button
-        addButton = eventNew.querySelector("button"); 
-        //add event listener to the add button
-        addButton.addEventListener("click", async (e) => {
-            e.preventDefault();
-            if(eventInput.value === "" || startDateInput.value === "" || endDateInput.value === "") {
-                alert("Please fill in all fields");
-                return;
-            }
-            const newEvent = {
-                eventName: eventInput.value,
-                startDate: startDateInput.value,
-                endDate: endDateInput.value,
-            };
 
-            const res = await eventAPI.postEvent(newEvent);
-            const events = await eventAPI.getEvents();
-            //remove all elements
-            eventNew.remove(eventInput);
-            eventNew.remove(startDateInput);
-            eventNew.remove(endDateInput);
-            eventNew.remove(addButton);
-            eventNew.remove(cancelButton);
-            renderEvents(events, "event-list");
-            return res.json;
-        });
-        eventNew.appendChild(addButton);
-        eventNew.appendChild(cancelButton);
-        const eventListElem = document.getElementById("event-new");
-        eventListElem.innerHTML = "";
-        eventListElem.appendChild(eventNew);
-
-    });
     //append all elements
     eventItemElem.appendChild(eventElem);
     eventItemElem.appendChild(startDateElem);
@@ -153,46 +113,93 @@ function createEventElem(event) {
 
     return eventItemElem;
 }
+function createNewEvent() {
+    //add new event button
+    const addNewEvent = document.getElementById("addNew");
+    const eventNew = document.createElement("tr");
+    const eventListElem = document.getElementById("event-new");
+    const eventInputAdd = document.createElement("input");
+    eventInputAdd.placeholder = "Event Name";
+    eventInputAdd.type = "text";
+    const tdEventInputAdd = document.createElement("td");
+    tdEventInputAdd.appendChild(eventInputAdd);
+    const startDateInputAdd = document.createElement("input");
+    startDateInputAdd.placeholder = "Start Date";
+    startDateInputAdd.type = "date";
+    const tdStartDateInputAdd = document.createElement("td");
+    tdStartDateInputAdd.appendChild(startDateInputAdd);
+    const endDateInputAdd = document.createElement("input");
+    endDateInputAdd.placeholder = "End Date";
+    endDateInputAdd.type = "date";
+    const tdEndDateInputAdd = document.createElement("td");
+    tdEndDateInputAdd.appendChild(endDateInputAdd);
+    const tdButtons = document.createElement("td");
+    const addButton = document.createElement("button");
+    addButton.textContent = "+";
+    tdButtons.appendChild(addButton);
+    const cancelButton = document.createElement("button");
+    cancelButton.textContent = "Cancel";
+    cancelButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        eventNew.remove();
+        eventListElem.innerHTML = "";
+        console.log(eventListElem.innerHTML);
+
+    });
+    tdButtons.appendChild(cancelButton);
+    //add event listener to the add button
+    addButton.addEventListener("click", async (e) => {
+        e.preventDefault();
+        if (eventInputAdd.value === "" || startDateInputAdd.value === "" || endDateInputAdd.value === "") {
+            alert("Please fill in all fields");
+            return;
+        }
+        const newEvent = { eventName: eventInputAdd.value, startDate: startDateInputAdd.value, endDate: endDateInputAdd.value };
+
+        const res = await eventAPI.postEvent(newEvent);
+        const events = await eventAPI.getEvents();
+        //remove all elements
+        eventNew.remove(eventInputAdd);
+        eventNew.remove(startDateInputAdd);
+        eventNew.remove(endDateInputAdd);
+        eventNew.remove(addButton);
+        eventNew.remove(cancelButton);
+        renderEvents(events, "event-list");
+        return res.json;
+    });
+    addNewEvent.addEventListener("click", async (e) => {
+        e.preventDefault();
+
+        eventNew.appendChild(tdEventInputAdd);
+        eventNew.appendChild(tdStartDateInputAdd);
+        eventNew.appendChild(tdEndDateInputAdd);
+        eventNew.appendChild(tdButtons);
+
+        eventListElem.innerHTML = "";
+        eventListElem.appendChild(eventNew);
+
+    });
+}
 //render the todos
 function renderEvents(events, type) {
     try {
         const eventListElem = document.getElementById(type);
-        
+
         eventListElem.innerHTML = ""; // clear the list
-        
+
         for (const event of events) {
             const eventElem = createEventElem(event);
             eventListElem.appendChild(eventElem);
         }
-    }catch (e) {
+    } catch (e) {
         console.error(e);
     }
-}
-//set form elements
-function setFormElements() {
-    const formElem = document.getElementById("event-form");
-    formElem.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        const eventName = document.getElementById("event-name").value;
-        const startDate = document.getElementById("start-date").value;
-        const endDate = document.getElementById("end-date").value;
-
-        const newEvent = {
-            eventName,
-            startDate,
-            endDate,
-        };
-
-        const res = await eventAPI.postEvent(newEvent);
-        const events = await eventAPI.getEvents();
-        renderEvents(events, "event-list");
-        return res.json;
-    });
 }
 //initialize the app
 (function initApp() {
     //setFormElements();
     createEventElem();
+    createNewEvent();
     eventAPI.getEvents().then((events) => {
         renderEvents(events, "event-list");
     });
